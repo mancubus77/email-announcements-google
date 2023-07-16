89b0d7c952b43ae03173ac8be17905f8d7cb07cd
Stan Kozlov <dont@write.me>
2023-07-16 14:39:20 +1000

    Add config examples

---
+++ configs/config-example.py
@@ -0,0 +1,10 @@
+# Google Spreadseet ID with emails
+SPREADSHEET_ID = "SoMeThIngLiKeThIs"
+# Name of Spreadsheet tab
+SPREADSHEET_TAB = "Tabname"
+# Email Subject
+SUBJ = "G'Day man, Rise and shine!"
+# Path to JSON with google oAuth
+CREDS_PATH = "google-creds/my-creds.json"
+# Template to use
+TEMPLATE = "announcements/example.html"
